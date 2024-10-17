770c52f8a1f8ec41e15ce86d750f6424ab428e10
kevinrosadigitalcodesign <kevin.rosa@digitalcodesign.com>
2024-10-17 07:48:14 +0100

    Created the LogicGates class skaffolding

---
+++ LogicGates.h
@@ -0,0 +1,31 @@
+/**
+ * LogicGates.h - Librería para compatibilizar funcionamiento del Entrenador de Puertas
+ * Logicas con MentorBit.
+ * 
+ * Creado el 17/10/2024 por Kevin Rosa - kevin.ingenieria@digitalcodesign.com
+ * 
+ * Declaración de atributos y métodos para la clase LogicGate, referida
+ * al Entrenador de Puertas Lógicas y las funcionalidades emuladas por software.
+ * 
+ * -> Timers.
+ * -> Contadores.
+ * -> Flip-Flops.
+ * -> Detectores de Flanco.
+ * -> Generador de Señales.
+ */
+
+#ifndef LogicGates_h
+#define LogicGates_h
+
+    #include "Arduino.h"
+
+    class LogicGates
+    {
+
+        public:
+
+            LogicGates();
+
+    }
+
+#endif
